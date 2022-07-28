779defd9f4f88c23b0024533c99ccb1f12448a52
B1N <choi44601280@gmail.com>
2022-07-28 22:17:42 +0900

    가로메뉴1유형

--- 01/01_prac/app.js
+++ 01/01_prac/app.js
@@ -1,7 +1,9 @@
 $(function(){
     $('.gnb > li').hover(function(){
-        $(this).find('.sub').stop().slideDown(500);
+        // $(this).find('.sub').stop().slideDown();
+        $('.sub').stop().slideDown();
     },function(){
-        $(this).find('.sub').stop().slideUp(500);
+        // $(this).find('.sub').stop().slideUp();
+        $('.sub').stop().slideUp();
     });
 })
